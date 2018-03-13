41ed5a11b92daa48100af31a012e6dcf1b0a8f22
Catalin Voss <catalin@cs.stanford.edu>
2018-03-12 22:48:11 -0700

    Get rid of gym envs -- god this is so messy…

--- train_linear.py
+++ train_linear.py
@@ -1,4 +1,3 @@
-import gym
 from utils.preprocess import greyscale
 from utils.wrappers import PreproWrapper, MaxAndSkipEnv
 
@@ -8,12 +7,8 @@ from linear_dqn import Linear
 from configs.linear import config
 
 if __name__ == '__main__':
-    # make env
-    env = gym.make(config.env_name)
-    env = MaxAndSkipEnv(env, skip=config.skip_frame)
-    env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1), 
-                        overwrite_render=config.overwrite_render)
-
+    env = None # TODO: pass in rewards model this way!
+    
     # exploration strategy
     exp_schedule = LinearExploration(env, config.eps_begin, 
             config.eps_end, config.eps_nsteps)
