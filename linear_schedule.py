457088eb7a46db02e98302d4aa5e3de7498a309a
Omer Hazon <omer.hazon@berkeley.edu>
2018-03-13 11:17:07 -0700

    bugfix linear_schedule

--- linear_schedule.py
+++ linear_schedule.py
@@ -59,7 +59,7 @@ class LinearExploration(LinearSchedule):
         if (rand_num <= self.epsilon):
             print self.epsilon
             print "e-greedy"
-            sample = np.random.randint(0, rewards.NUM_ACTIONS)
+            sample = np.random.randint(0, self.env.num_actions)
             return sample
         else:
             return best_action
