5c2a4ad3a80e8d03d7c14e294b5b06614dd2a362
Catalin Voss <catalin@cs.stanford.edu>
2018-03-22 16:08:51 -0700

    Add examples

--- replay_music.py
+++ replay_music.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     dj = NeuralDJ(read_midis.NUM_NOTES, read_midis.BAR_QUANT, read_midis.ROLL_WINDOW)
     dj.start_playback()
 
-    bars = np.load("rahulsong.npy") # catalins_funk.npy, wobble.npy, rahulsong.npy, fromzerotoone.npy, sowrongsoright.npy, lotta_harmony.npy
+    bars = np.load("examples/rahulsong.npy") # catalins_funk.npy, wobble.npy, rahulsong.npy, fromzerotoone.npy, sowrongsoright.npy, lotta_harmony.npy
 
     for bar in bars:
         print("MIDI-bar")
